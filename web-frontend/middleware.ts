d9abc64d5fcb7e3ae84b23db1a1ccfe28f1b3318
saqibmurtaza <saqibmurtazakhan@gmai.com>
2025-08-18 17:51:55 +0500

    move to web-frontend root from src

---
+++ web-frontend/middleware.ts
@@ -0,0 +1,20 @@
+// // middleware.ts
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//   clockSkewInMs: 30000, // Optional: handles minor time drift
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
+// middleware.ts at root
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
