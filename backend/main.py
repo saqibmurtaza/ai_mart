51473b652b4c996da438f75670b94eb322faed64
saqibmurtaza <saqibmurtazakhan@gmai.com>
2025-08-20 15:39:24 +0500

    Add ['*'] in CORS

--- backend/main.py
+++ backend/main.py
@@ -77,8 +77,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods="",
-    allow_headers="", 
+    allow_methods=["*"],
+    allow_headers=["*"], 
 )
 
 
